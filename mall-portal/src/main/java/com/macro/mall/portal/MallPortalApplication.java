62b49de2ee43b598c60a4f4fee9f917fed8fe09f
LincolnKong <274106916@qq.com>
2020-09-21 15:40:15 +0800

    test

--- mall-portal/src/main/java/com/macro/mall/portal/MallPortalApplication.java
+++ mall-portal/src/main/java/com/macro/mall/portal/MallPortalApplication.java
@@ -1,7 +1,10 @@
 package com.macro.mall.portal;
 
+import javafx.application.Application;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication(scanBasePackages = "com.macro.mall")
 public class MallPortalApplication {
